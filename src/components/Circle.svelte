fa67dfd825058247ef22faff397c3d99c524d5e7
Yoric Mangeart <yoricm@free.fr>
2022-05-23 08:14:09 +0200

    show impact anim css

--- src/components/Circle.svelte
+++ src/components/Circle.svelte
@@ -4,32 +4,51 @@
     ypx = "";
   let radius = 100;
   let radiuspx = `${radius}px`;
+  let hit = false;
+  let width;
+  let height;
+  const elt = document.querySelector(".hit");
   const newPos = () => {
+    //elt.style.display = "none";
+    radius = 100 + Math.round(Math.random() * 200);
     pos = {
-      x: Math.round(Math.random() * 600),
-      y: Math.round(Math.random() * 400),
+      x: Math.round(Math.random() * (width - radius * 2)),
+      y: Math.round(Math.random() * (height - radius * 2)),
     };
     xpx = pos.x + "px";
     ypx = pos.y + "px";
-    radius = 100 + Math.round(Math.random() * 200);
     radiuspx = `${radius}px`;
-    console.log(radiuspx);
+    hit = false;
   };
   newPos();
   // left = "300px";
 
   const handleClick = (event) => {
+    hit = true;
+    const elt = document.querySelector("#clc");
+
+    elt.classList.add("hit");
+    elt.style.left = event.x - 0 + "px";
+    elt.style.top = event.y - 0 + "px";
+    elt.style.display = "block";
+    //elt.classList.remove("run-animation");
+    //void elt.offsetWidth;
+
     setTimeout(() => {
+      elt.style.display = "none";
       newPos();
-    }, 500);
+    }, 1000);
   };
 </script>
 
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
 <div
   on:click={handleClick}
-  class="circle"
+  class="circle "
   style="--xpx:{xpx};--ypx:{ypx};--radiuspx:{radiuspx};"
-/>
+>
+  <div id="clc" />
+</div>
 
 <style>
   .circle {
